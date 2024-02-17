e9590ec2e2d0adc21f54245005e27ce502bb0d6e
Tannafama <143949496+tannafama@users.noreply.github.com>
2024-02-17 10:26:02 +0100

    Create code_3.py

---
+++ code_3.py
@@ -0,0 +1,16 @@
+        console.log("Random Number: " + item);
+}
+    const data = generateRandomData();
+    data.forEach(item => {
+        data.push(Math.floor(Math.random() * 100) + 1);
+    }
+
+
+    for (let i = 0; i < 10; i++) {
+function generateRandomData() {
+function main() {
+    return data;
+    const data = [];
+}
+    });
+main();
